32437b82bb28c1f4441e3f2c53b37f648747f29d
Noyan Berkay Aşkın <berkay4.askin@hotmail.com>
2022-07-30 14:51:07 +0300

    Indirimli ürünler modeli eklendi

---
+++ AnimeShop/Models/IndirimliUrunler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+namespace AnimeShop.Models
+{
+    public class IndirimliUrunler
+    {
+        public int Id { get; set; }
+
+        public int? UrunId { get; set; }
+        [ForeignKey("UrunId")]
+        public Urun Urun { get; set; }
+        public double Oran { get; set; }
+        public DateTime Baslangic { get; set; }
+        public DateTime Bitis { get; set; }//indirimin olacağı tarih aralığı
+        public bool DigerKampanya { get; set; } = true; //ekstra olacak bir kampanya da olursa o da işlensin mi?
+    }
+}
