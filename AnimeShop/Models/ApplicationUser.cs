88eb3ed09407b34fbe2b4c63b7c4c99e19078770
Noyan Berkay Aşkın <berkay4.askin@hotmail.com>
2022-07-31 13:45:37 +0300

    ApplicationUser modeli eklendi
    Kullanıcı verilerini burada tutuyoruz

---
+++ AnimeShop/Models/ApplicationUser.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+namespace AnimeShop.Models
+{
+    public class ApplicationUser:IdentityUser
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public string Adres { get; set; }
+        public string Telefon { get; set; }
+        public DateTime DogumTarihi { get; set; }
+        public DateTime UyeOlmaTarihi { get; set; }
+        public Cinsiyet Cinsiyet { get; set; }
+        public string Sehir { get; set; }
+        [NotMapped]
+        public string Username
+        {
+            get
+            {
+                return Ad + " " + Soyad;
+            }
+        }
+
+    }
+
+
+    public enum Cinsiyet
+    {
+        Kadin,
+        Erkek,
+        Hiçbiri
+    }
+}
+
