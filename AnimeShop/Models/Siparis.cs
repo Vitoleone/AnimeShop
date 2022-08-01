bf1150820ffd1ee20833b5f575a57a428e0b6179
Noyan Berkay Aşkın <berkay4.askin@hotmail.com>
2022-08-01 19:47:01 +0300

    Siparis. modeli eklendi
    siparisle ilgili ana bilgileri içeren model eklendi.

---
+++ AnimeShop/Models/Siparis.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AnimeShop.Models
+{
+    public class Siparis
+    {
+        public int Id { get; set; }
+        public string MusteriId { get; set; }//Musteri nin id primary key i string olarak tutulduğundan string dedim.
+        [ForeignKey("MusteriId")]
+        public ApplicationUser ApplicationUser { get; set; }//bunu da foreign için ekledim
+        public DateTime SiparisTarihi { get; set; }
+        public DateTime? GondermeTarihi { get; set; }
+        public DateTime? TeslimTarihi { get; set; }
+        public DateTime? IadeTarihi { get; set; }
+        public string KargoFirma { get; set; }
+        public double KargoUcreti { get; set; }
+        public double ToplamUcret { get; set; }
+        public double Indirim { get; set; }
+        public SiparisDurumu SiparisDurumu { get; set; }
+        public OdemeDurumu OdemeDurumu { get; set; }
+
+        public string SiparisKodu { get; set; }
+        public string KargoTakipNo { get; set; }
+        public string Aciklama { get; set; }
+
+
+    }
+    public enum SiparisDurumu
+    {
+        Hazirlaniyor,
+        KargoyaVerildi,
+        TeslimEdildi,
+        İadeEdildi
+    }
+    public enum OdemeDurumu
+    {
+        KrediKarti,
+        Eft,
+        Havale,
+        KapidaOdeme,
+        Onaylanmadi
+    }
+}
+
