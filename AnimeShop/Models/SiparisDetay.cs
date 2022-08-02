0d9cfd77bfcb581920a42e338e6a4179e1bc14ba
Noyan Berkay Aşkın <berkay4.askin@hotmail.com>
2022-08-02 22:07:51 +0300

    SiparisDetay modeli eklendi
    Bu modelde siparişimizle ilgili fiyat,miktar gibi değerler tutulur.

---
+++ AnimeShop/Models/SiparisDetay.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AnimeShop.Models
+{
+    public class SiparisDetay
+    {
+        public int Id { get; set; }
+
+        public int? SiparisId { get; set; }//Foreign keylerde eşleşme olmazsa diye null olabilme ihtimaline izin verdik
+        [ForeignKey("SiparisId")]
+        public Siparis Siparis { get; set; }
+
+        public int UrunId { get; set; }
+        [ForeignKey("UrunId")]
+        public Urun Urun { get; set; }
+
+        public double Miktar { get; set; }
+        public double Fiyat { get; set; }
+
+        [NotMapped]//bunu yazıyorsak veritabanında aşağıdaki alanı oluşturmaz yani ücreti oluşturmayacaz çünkü veritabanındaki 2 değişkenden türüyor.
+        public double Ucret
+        {
+            get
+            {
+                return Fiyat * Miktar;
+            }
+        }
+    }
+}
