ad5220ef0e239b6aa9af72719b72830bd063c50c
Noyan Berkay Aşkın <berkay4.askin@hotmail.com>
2022-08-12 14:52:30 +0300

    KategoriUrun viewComponent eklendi
    kategori detay sayfasında ilgili kategorideki ürünlerin adını, miktarını, fiyatını ve açıklamasını gösterir.

---
+++ AnimeShop/Components/KategoriUrun.cs
@@ -0,0 +1,23 @@
+﻿using AnimeShop.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace AnimeShop.Components
+{
+    public class KategoriUrun:ViewComponent
+    {
+
+        private readonly ApplicationDbContext _context;
+        public KategoriUrun(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IViewComponentResult Invoke(int id)//bütün viewComponentlerde tanımlanır, id hangi ürünlerin kategorisini getireceğimizi belirler.
+        {
+            var liste = _context.Urun.Include(p => p.Kategori).Where(x => x.KategoriId == id);//Kategori tablosunda olan ürünleri getirir
+            return View(liste);//liste modelini view e gönderir.
+            //bu viewcomponent in html sini views içerisindeki Shared altındaki Components klasörüne KategoriUrun diye başka bir klasör oluşturur ve orada tutarız
+        }
+    }
+}
