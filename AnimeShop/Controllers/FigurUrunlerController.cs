033f87a115f3fa3cb661fcc681cf7f6468914de4
Noyan Berkay Aşkın <berkay4.askin@hotmail.com>
2022-08-18 15:14:46 +0300

    Ürünler düzenleme
    Artık ürünler sekmesindeki figür kategorisini seçtiğimde sadece figürler geliyor

---
+++ AnimeShop/Controllers/FigurUrunlerController.cs
@@ -0,0 +1,42 @@
+﻿using AnimeShop.Data;
+using AnimeShop.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AnimeShop.Controllers
+{
+    public class FigurUrunlerController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public FigurUrunlerController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var urunList = (from u in _context.Urun
+                            join k in _context.Kategori on u.KategoriId equals k.Id
+                            join f in _context.Fotograf on u.Id equals f.UrunId
+                            join i in _context.IndirimliUrunler on u.Id equals i.UrunId
+                            select new UrunDTO
+                            {
+                                UrunId = u.Id,
+                                UrunAdi = u.Ad,
+                                UrunFiyat = u.Fiyat,
+                                UrunFotograf = f.ResimAd,
+                                UrunMiktar = u.Miktar,
+                                UrunKategori = k.Ad,
+                                UrunIndirimOrani = i.Oran
+
+
+                            }).ToList();
+            return View(urunList.Where(k => k.UrunKategori == "Figür"));
+        }
+    }
+}
